39416fe7fa8f0eccfac1bec6ef1a00173ee8f54b
hristina-trumpic <33160209+hristina-trumpic@users.noreply.github.com>
2017-10-27 19:35:40 +0200

    1st up
    test with google maps API (just an idea)

--- js.js
+++ js.js
@@ -1,1 +1,7 @@
-alert('working');
+  var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 43.320384, lng: 21.903424},
+      zoom: 15
+    });
+  }
